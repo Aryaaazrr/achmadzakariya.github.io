528e8a9dd063e788a3c1f65d9ffe0b3e6a600bc9
Aryaaazrr <achmadzakariya7@gmail.com>
2023-02-27 22:31:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      colors: {
+        primary: '#06b6d4',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      },
+    },
+  },
+  plugins: [],
+}
